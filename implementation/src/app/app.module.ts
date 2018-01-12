00013a2b812a67ebf9c20b24eae62e4ac00a40ce
lucasgian <gianextensao@gmail.com>
2018-01-12 15:01:07 -0300

    correções

--- implementation/src/app/app.module.ts
+++ implementation/src/app/app.module.ts
@@ -1,3 +1,4 @@
+//import { ServiceWorkerModule } from '@angular/service-worker';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
--- implementation/src/main.ts
+++ implementation/src/main.ts
@@ -7,6 +7,16 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
-
+/*
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
+*/
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .then(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/ngsw-worker.js') ;
+    }
+  })
+  .catch(err => console.log(err));
+  
